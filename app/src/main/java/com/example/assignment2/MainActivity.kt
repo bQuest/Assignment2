28e248ced37f86d674cfa548fe2143292a83cd97
Quest <b.quest@hotmai.com>
2022-02-20 18:29:41 -0500

    add food

--- app/src/main/java/com/example/assignment2/MainActivity.kt
+++ app/src/main/java/com/example/assignment2/MainActivity.kt
@@ -1,11 +1,25 @@
 package com.example.assignment2
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_main.*
+
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    var menu = ArrayList<String>(arrayListOf("Hamburger", "Pizza", "Mexican", "American", "Chinese"))
+
+    fun decideFood(view: View) {
+        var index: Int = (Math.random() * menu?.size).toInt()
+        textView_displayFood.text = menu[index]
+    }
+    fun addFood(view: View) {
+        val foodName = editText_FoodName.text.toString()
+        menu.add(foodName)
     }
 }
